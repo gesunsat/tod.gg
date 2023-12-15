596fd20cc95d5205ae8391fc4b5b24bac2fe05b5
한상길 <sjssj7777@naver.com>
2023-12-15 23:51:03 +0900

    [ADD] 다크모드 기본 세팅

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Providers from './providers'
 
 export const metadata = {
   title: 'TOD.GG',
@@ -8,7 +9,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="kr">
-      <body>{children}</body>
+      <body>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
     </html>
   )
 }
