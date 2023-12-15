249257fd748a0e2982fd8377f6e3a4fcb7ba44c3
한상길 <sjssj7777@naver.com>
2023-12-16 07:21:06 +0900

    [ADD] 폰트 추가

--- app/layout.js
+++ app/layout.js
@@ -3,20 +3,24 @@ import Footer from '@/components/footer'
 import './globals.css'
 import Providers from './providers'
 import Container from '@/components/container'
+import { Noto_Sans_KR } from 'next/font/google';
+
+const inter = Noto_Sans_KR({ subsets: ["latin"] });
 
 export const metadata = {
   title: 'TOD.GG',
   description: '메이플 캐릭터 조회',
 }
 
+//TODO: 다크 모드 씨발것 오류 없애기
 export default function RootLayout({ children }) {
   return (
     <html lang="kr">
-      <body>
+      <body className={inter.className}>
         <Providers>
           <Header />
 
-          <Container large>{children}</Container>
+          <Container>{children}</Container>
 
           <Footer />
         </Providers>
