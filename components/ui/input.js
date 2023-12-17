645341efee7025303850839680c43c13e6a2df36
한상길 <sjssj7777@naver.com>
2023-12-18 03:03:13 +0900

    [CHANGE] Footer

--- components/ui/input.js
+++ components/ui/input.js
@@ -1,6 +1,6 @@
 import { cx } from "@/utils/all";
 
-export default function input({ className, ...props }) {
+export default function Input({ className, ...props }) {
   return (
     <input
       className={cx("rounded-md ring-1 ring-gray-400 placeholder:text-gray-400 sm:leading-6", className)}
