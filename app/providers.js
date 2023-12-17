71782218aa6a9094221475f4c32854377e4ea61e
한상길 <sjssj7777@naver.com>
2023-12-17 20:06:22 +0900

    [CHANGE] 다크 모드

--- app/providers.js
+++ app/providers.js
@@ -1,8 +1,18 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function Providers({ children }) {
+  const [mounted, setMounted] = useState(false)
+
+  // TODO: setMount 뭔가 쓰는거 아닌거같은데
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) return null;
+
   return (
     <ThemeProvider attribute="class" enableSystem={true}>
       {children}
--- components/themeSwitch.js
+++ components/themeSwitch.js
@@ -4,17 +4,35 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import MoonIcon from "@/public/moon.svg";
 import SunIcon from "@/public/sun.svg";
+import { useEffect, useState } from "react";
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme
+  const [mounted, setMounted] = useState(false)
+
+  // TODO: setMount 뭔가 쓰는거 아닌거같은데
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  if (!mounted) return null;
 
   return (
-    <Image
-        src={currentTheme === 'dark' ? MoonIcon : SunIcon}
+    <div>
+      <Image
+        className="hidden dark:invert dark:block"
+        src={MoonIcon}
         alt="테마 변경"
-        onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}
+        onClick={() => setTheme("light")}
         width={50}
-    />
+      />
+      <Image
+        className="invert-0 dark:hidden"
+        src={SunIcon}
+        alt="테마 변경"
+        onClick={() => setTheme("dark")}
+        width={50}
+      />
+    </div>
   );
 };
