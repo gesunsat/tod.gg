9aea3c38623d5fb25b78b598c6cfc96073c0f6f8
한상길 <sjssj7777@naver.com>
2023-12-22 02:10:21 +0900

    [CHANGE] themeSwitch > css 지역화

--- components/themeSwitch.js
+++ components/themeSwitch.js
@@ -5,6 +5,8 @@ import Image from "next/image";
 import MoonIcon from "@/public/moon.svg";
 import SunIcon from "@/public/sun.svg";
 import { useEffect, useState } from "react";
+import Style from './themeSwitch.module.css';
+import { cx } from "@/utils/all";
 
 export default function ThemeSwitch({ className, ...props }) {
   const { theme, setTheme } = useTheme();
@@ -20,14 +22,14 @@ export default function ThemeSwitch({ className, ...props }) {
   return (
     <div className={className}>
       <Image
-        className="shakeAnime cursor-pointer hidden dark:invert dark:block"
+        className={cx("shakeAnime cursor-pointer hidden dark:invert dark:block", Style.shakeAnime)}
         src={MoonIcon}
         alt="테마 변경"
         onClick={() => setTheme("light")}
         width={props.height}
       />
       <Image
-        className="shakeAnime cursor-pointer invert-0 dark:hidden"
+        className={cx("shakeAnime cursor-pointer invert-0 dark:hidden", Style.shakeAnime)}
         src={SunIcon}
         alt="테마 변경"
         onClick={() => setTheme("dark")}
