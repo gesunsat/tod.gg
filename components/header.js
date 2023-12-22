d9eecc7c8167ed1de2c1d75ed58b54f9b32a154e
한상길 <sjssj7777@naver.com>
2023-12-22 17:28:27 +0900

    [CHANGE] header

--- components/header.js
+++ components/header.js
@@ -1,24 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Container from './container'
-import ThemeSwitch from "./themeSwitch";
-import Logo from "@/public/logo.svg";
+import ThemeSwitch from './themeSwitch';
+import Logo from '@/public/logo.svg';
 import SearchCharcter from './rcc/searchCharacter';
+import Nav from './nav';
 
 export default function Header() {
   return (
     <header className="w-full">
       <Container className="flex py-5 flex-col sm:flex-row">
         <div>
-          <Link href="/" className="block mb-3">
-            <Image className="mx-auto hidden dark:invert dark:block" src={Logo} alt="TOD.GG" height={40} />
-            <Image className="mx-auto dark:invert-0 dark:hidden" src={Logo} alt="TOD.GG" height={40} />
+          <ThemeSwitch className="absolute top-5 right-3 block sm:hidden" height="20" />
+          <Link href={"/"}>
+            <Image className="mx-auto w-auto h-[20px] sm:h-[40px] mb-5 sm:mb-0 hidden dark:invert dark:block" src={Logo} alt="TOD.GG" />
+            <Image className="mx-auto w-auto h-[20px] sm:h-[40px] mb-5 sm:mb-0 dark:invert-0 dark:hidden" src={Logo} alt="TOD.GG" />
           </Link>
-          <ThemeSwitch className="absolute top-6 right-3 block sm:hidden" height="30" />
         </div>
         <SearchCharcter />
         <ThemeSwitch className="me-3 hidden sm:block" height="40" />
       </Container>
-    </header >
+      <Nav />
+    </header>
   )
 }
