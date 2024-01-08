25cbbf69aa96131ec6b085ec71eb1c9c0650a8b1
한상길 <sjssj7777@naver.com>
2024-01-09 01:55:10 +0900

    google, naver Analytics, clarity 추가

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Providers from './providers'
 import { Noto_Sans_KR } from 'next/font/google';
 import { cn } from "@/lib/utils"
 import { Toaster } from '@/components/ui/toaster';
+import Script from 'next/script';
 
 const interNotoSansKR = Noto_Sans_KR({ subsets: ["latin"] });
 
@@ -45,6 +46,37 @@ export default function RootLayout({ children }) {
           </div>
         </Providers>
         <Toaster />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TDN2JFVM58" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-TDN2JFVM58');
+          `}
+        </Script>
+
+        <Script async src="//wcs.naver.net/wcslog.js" />
+        <Script id="naver-analytics">
+          {`
+            if(!wcs_add) var wcs_add = {};
+            wcs_add["wa"] = "1fc1d575d28b680";
+            if(window.wcs) {
+              wcs_do();
+            }
+          `}
+        </Script>
+        <Script id="clarity">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "kiq42gxpes");
+          `}
+        </Script>
       </body>
     </html>
   )
