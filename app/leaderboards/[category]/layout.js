668edbb032c9864b17a7d9067bcb758d2d58bebd
한상길 <sjssj7777@naver.com>
2024-02-17 22:01:48 +0900

    랭킹 페이지 메타 데이터 수정

--- app/leaderboards/[category]/layout.js
+++ app/leaderboards/[category]/layout.js
@@ -4,64 +4,40 @@ import LeaderboardsServerList from "./serverList";
 import LeaderboardsTabs from "./tabs";
 import { Skeleton } from "@/components/ui/skeleton";
 import LeaderboardsGuildTabs from "./guildTabs";
+import { searchQueryParams } from "@/lib/getQueryParam";
+import { headers } from "next/headers";
 
-// export async function generateMetadata({ params }) {
-//     try {
-//         const headersList = headers();
-//         const header_url = headersList.get("x-url") || "";
-//         const selectedDate = searchQueryParams({ query: "date", fullUrl: header_url });
-//         const OCID = await getCharOCID(decodeURI(params.name));
+const categoryKorean = {
+    "power": "전투력",
+    "overall": "종합",
+    "guild": "길드",
+    "union": "유니온",
+    "dojang": "무릉도장",
+    "achievement": "업적",
+    "theseed": "더 시드",
+}
+const guildRankingTypeKorean = {
+    "suro": "지하 수로",
+    "flag": "플래그",
+    "fame": "주간 명성치",
+}
 
-//         if (!OCID?.ocid) {
-//             return {
-//                 title: "캐릭터 정보 | TOD.GG",
-//                 description: "캐릭터가 존재하지 않거나 불러올 수 없는 캐릭터입니다.",
-//             }
-//         }
+export async function generateMetadata({ params, searchParams }) {
+    const headersList = headers();
+    const header_url = headersList.get("x-url") || "";
+    const server = decodeURI(searchQueryParams({ query: "server", fullUrl: header_url }) || "전섭").replace("+", " ");
+    const job = decodeURI(searchQueryParams({ query: "job", fullUrl: header_url }) || "");
+    const type = decodeURI(searchQueryParams({ query: "type", fullUrl: header_url }) || "suro");
 
-//         const [characterBasic, characterStat, userUnion] = await Promise.all([
-//             getCharBasic(OCID.ocid, selectedDate),
-//             getCharStat(OCID.ocid, selectedDate),
-//             getUserUnion(OCID.ocid, selectedDate),
-//         ]);
-
-//         if (!characterBasic.character_class) {
-//             return {
-//                 title: "캐릭터 정보 | TOD.GG",
-//                 description: "캐릭터가 존재하지 않거나 불러올 수 없는 캐릭터입니다.",
-//             }
-//         }
-
-//         return {
-//             title: `${characterBasic.character_name} 캐릭터 정보 | TOD.GG 토드지지`,
-//             description:
-//                 `${characterBasic.world_name} | ` +
-//                 `${characterBasic.character_class} | ` +
-//                 `Lv.${characterBasic.character_level} | ` +
-//                 `길드:${characterBasic.character_guild_name ? characterBasic.character_guild_name + " | " : "- | "}` +
-//                 `전투력:${characterStat?.final_stat.reverse().map((stat) => { if (stat.stat_name == "전투력") return parseInt(stat.stat_value).toLocaleString() }).join('')} | ` +
-//                 `${userUnion?.union_level ? "유니온:" + userUnion?.union_level : ""}`,
-//             openGraph: {
-//                 images: characterBasic.character_image,
-//                 url: `https://tod.gg/char/${params.id}`,
-//                 title: `${characterBasic.character_name} 캐릭터 정보 | TOD.GG`,
-//                 description:
-//                     `${characterBasic.world_name} | ` +
-//                     `${characterBasic.character_class} | ` +
-//                     `Lv.${characterBasic.character_level} | ` +
-//                     `길드:${characterBasic.character_guild_name ? characterBasic.character_guild_name + " | " : "- | "}` +
-//                     `전투력:${characterStat?.final_stat.reverse().map((stat) => { if (stat.stat_name == "전투력") return parseInt(stat.stat_value).toLocaleString() }).join('')} | ` +
-//                     `${userUnion?.union_level ? "유니온:" + userUnion?.union_level : ""}`,
-//             }
-//         }
-//     } catch (err) {
-//         console.log(err);
-//         return {
-//             title: "캐릭터 정보 | TOD.GG",
-//             description: "캐릭터가 존재하지 않거나 불러올 수 없는 캐릭터입니다.",
-//         }
-//     }
-// }
+    return {
+        title: `${server != "전섭" ? `${server} 서버` : server}${job ? ` ${job}` : ""} ${categoryKorean[params.category]}${params.category == "guild" && type ? ` ${guildRankingTypeKorean[type]}` : ""} 랭킹 | TOD.GG 토드지지`,
+        openGraph: {
+            images: "https://tod.gg/tode_hammer.png",
+            url: `https://tod.gg/leaderboards/${params.category}`,
+            title: `${server != "전섭" ? `${server} 서버` : server}${job ? ` ${job}` : ""} ${categoryKorean[params.category]}${params.category == "guild" && type ? ` ${guildRankingTypeKorean[type]}` : ""} 랭킹 | TOD.GG 토드지지`,
+        }
+    }
+}
 
 export default async function CharacterLayout({ params, searchParams, children }) {
     return (
