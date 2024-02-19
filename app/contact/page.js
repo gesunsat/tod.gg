a9143bb2851dc211df5fbce6d8704217e0906ba9
한상길 <sjssj7777@naver.com>
2024-02-20 06:27:13 +0900

    수익좀 나기 시작할 때까지 문의 메일은 네이버 메일로

--- app/contact/page.js
+++ app/contact/page.js
@@ -6,7 +6,8 @@ export default function Contact() {
             <div className="flex flex-col justify-center place-items-center h-full space-y-5">
                 <div>
                     <span>이메일 : </span>
-                    <Link href="mailto:admin@tod.gg">admin@tod.gg</Link>
+                    {/* <Link href="mailto:admin@tod.gg">admin@tod.gg</Link> */}
+                    <Link href="mailto:todgg@naver.com">todgg@naver.com</Link>
                 </div>
                 <div>
                     <span>디스코드 : </span>
