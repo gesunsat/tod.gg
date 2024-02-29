837bd85a5c5e363a3cd410329cde38db6f00c0e8
한상길 <sjssj7777@naver.com>
2024-03-01 06:43:52 +0900

    로깅 조건부 추가

--- middleware.js
+++ middleware.js
@@ -4,24 +4,26 @@ export async function middleware(request) {
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set('x-url', request.url);
 
-    try {
-        const logData = {
-            "method": request.method,
-            "url": request.url,
-            "ip": requestHeaders.get("x-forwarded-for"),
-            "headers": JSON.stringify(Object.fromEntries(requestHeaders.entries())),
-            "query": JSON.stringify(Object.fromEntries(request.nextUrl.searchParams.entries()))
-        };
+    if (!requestHeaders.get("x-amzn-trace-id")) {
+        try {
+            const logData = {
+                "method": request.method,
+                "url": (request.url).replace("https://ip-10-0-7-134.ap-northeast-2.compute.internal:3000", ""),
+                "ip": requestHeaders.get("x-forwarded-for"),
+                "headers": JSON.stringify(Object.fromEntries(requestHeaders.entries())),
+                "query": JSON.stringify(Object.fromEntries(request.nextUrl.searchParams.entries()))
+            };
 
-        const url = new URL(`${process.env.NEXT_PUBLIC_TOD_API_HOST}/scr/logging`);
-        const params = { "log": JSON.stringify(logData) };
-        url.search = new URLSearchParams(params).toString();
-        const option = {
-            method: "POST",
-            headers: { "accept": "application/json" }
-        };
-        fetch(url, option);
-    } catch (e) { console.log(e); }
+            const url = new URL(`${process.env.NEXT_PUBLIC_TOD_API_HOST}/scr/logging`);
+            const params = { "log": JSON.stringify(logData) };
+            url.search = new URLSearchParams(params).toString();
+            const option = {
+                method: "POST",
+                headers: { "accept": "application/json" }
+            };
+            fetch(url, option);
+        } catch (e) { console.log(e); }
+    }
 
     return NextResponse.next({
         request: {
